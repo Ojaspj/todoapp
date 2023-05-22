0d2709929b7dacd64641747085831f5c8a0446f8
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-05-22 17:17:14 +0545

    todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { HStack, IconButton, Text, VStack } from '@chakra-ui/react'
+import { HStack, IconButton, StackDivider, Text, VStack, Spacer } from '@chakra-ui/react'
 import React from 'react'
 import {FaTrash} from 'react-icons/fa'
 
@@ -14,10 +14,19 @@ const TodoList = () => {
         },
     ];
   return (
-    <VStack>
+    <VStack divider={<StackDivider/>} 
+            borderColor={'gray.200'} 
+            borderWidth={2} 
+            borderRadius={'lg'} 
+            p={4} 
+            w={'100%'}
+            maxW={{base: '90vw', sm: '80vw', lg:'50vw', xl:'40vw'}}
+            alignItems={'stretch'}
+            >
         {todos.map(todo => (
-            <HStack>
+            <HStack key={todo.id}>
                 <Text>{todo.body}</Text>
+                <Spacer />
                 <IconButton icon={<FaTrash />} isRound='true' />
             </HStack>
         ))}
